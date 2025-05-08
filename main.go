26b430afb529df0770f6ce060b3196ace41669ab
justinbornais <justin.bornais@gmail.com>
2025-05-08 14:04:59 -0400

    Now embeds entire static folder.

--- main.go
+++ main.go
@@ -8,19 +8,16 @@ import (
 	"github.com/justinbornais/go-directory-tracer/utilities"
 )
 
-//go:embed static/style.css
-var CSSData embed.FS
-
-//go:embed static/script.js
-var JSData embed.FS
+//go:embed static
+var StaticFiles embed.FS
 
 func main() {
 	// Get from command line.
 	title := flag.String("title", "Sample Website", "Specify the title of the website")
 	flag.Parse()
 
-	css := utilities.GetCSS(CSSData)
-	js := utilities.GetJS(JSData)
+	css := utilities.GetCSS(StaticFiles)
+	js := utilities.GetJS(StaticFiles)
 	ignored, err := utilities.ReadFileIgnore("./.fileignore")
 	utilities.CheckError(err)
 
